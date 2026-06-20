f1ce5232edc7af0f8dceaf7157fb2d8598818a28
SamuBury007 <samybury07@gmail.com>
2026-06-20 16:41:26 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 set -e
+
+# Aggiorna pip e installa i pacchetti Python requisiti
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Installa il browser Chromium e tutte le librerie di sistema Linux necessarie per farlo girare headless
+playwright install chromium
+playwright install-deps chromium
