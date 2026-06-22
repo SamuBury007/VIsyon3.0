5515d023eed961ff760326847583d596c20072d9
SamuBury007 <samybury07@gmail.com>
2026-06-22 19:01:34 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Aggiorna pip e installa i pacchetti Python
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Installa SOLO il binario di Chromium senza i pacchetti di sistema (APT)
-playwright install chromium
